2bcf261e787d752c4b7afcf35d1cd0c0898531b2
Devadeth shaji <devadethofficial@gmail.com>
2025-05-06 14:35:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./components/Card";
+
+export default function App() {
+
+  return(
+    <div>
+      
+      <div>
+
+      <Card title='Nesa' content='Sinro contents based on the Embeded System and the Ai ML robitics based classes , python react , data analytics etc ' />
+      
+      </div>
+
+      
+    </div>
+  )
+}
+
